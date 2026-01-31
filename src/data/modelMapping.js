b20e2d1bcdd09a739b640bd31c517dced83f21dc
张三 <2869472750@qq.com>
2026-01-31 10:53:36 +0800

    feat: 模型文件名英文化 + 新增118个角色模型
    - 重命名118个中文模型文件为英文名称
    - 新增崩坏星穹铁道角色: Bronya, Himeko, Klara, Natasha, Sampo, Welt等
    - 新增原神角色: Seele, Celestia, Dottore, La Signora等
    - 创建modelMapping.js映射表(中文名<->英文名)
    - 更新modelList.js使用英文名并添加source/avatar字段
    - 支持按游戏来源分类(原神/崩坏)

---
+++ src/data/modelMapping.js
@@ -0,0 +1,163 @@
+// 模型文件名映射表 - 中文名到英文名
+export const modelNameMapping = {
+  // 原神角色
+  '七七': 'Qiqi',
+  '七七2': 'Qiqi2',
+  '万叶': 'Kazuha',
+  '丽莎 (2)': 'Lisa2',
+  '丽莎': 'Lisa',
+  '久岐忍': 'KukiShinobu',
+  '九条裟罗 (2)': 'KujouSara2',
+  '九条裟罗': 'KujouSara',
+  '云堇': 'YunJin',
+  '云堇2': 'YunJin2',
+  '五郎 (1)': 'Gorou1',
+  '五郎 (2)': 'Gorou2',
+  '优菈 (1)': 'Eula1',
+  '优菈 (2)': 'Eula2',
+  '八重神子': 'YaeMiko',
+  '八重神子？': 'YaeMikoAlt',
+  '公子': 'Tartaglia',
+  '公子2': 'Tartaglia2',
+  '凝光': 'Ningguang',
+  '凝光2': 'Ningguang2',
+  '凯亚 (1)': 'Kaeya1',
+  '凯亚 (2)': 'Kaeya2',
+  '刻晴 (1)': 'Keqing1',
+  '刻晴 (2)': 'Keqing2',
+  '北斗': 'Beidou',
+  '北斗2': 'Beidou2',
+  '博士（多托雷）': 'Dottore',
+  '卡维': 'Kaveh',
+  '可莉': 'Klee',
+  '坎蒂丝': 'Candace',
+  '多莉': 'Dori',
+  '夜兰': 'Yelan',
+  '女士': 'LaSignora',
+  '妮露': 'Nilou',
+  '安柏': 'Amber',
+  '安柏2': 'Amber2',
+  '希儿': 'Seele',
+  '戴因斯雷布 (1)': 'Dainsleif1',
+  '戴因斯雷布 (2)': 'Dainsleif2',
+  '托马': 'Thoma',
+  '托马2': 'Thoma2',
+  '提纳里': 'Tighnari',
+  '散兵': 'Scaramouche',
+  '散兵2': 'Scaramouche2',
+  '早柚 (2)': 'Sayu2',
+  '早柚': 'Sayu',
+  '枫原万叶': 'KaedeharaKazuha',
+  '柯莱': 'Collei',
+  '派蒙': 'Paimon',
+  '流浪者': 'Wanderer',
+  '温迪': 'Venti',
+  '温迪2': 'Venti2',
+  '温迪3': 'Venti3',
+  '烟绯': 'Yanfei',
+  '烟绯2': 'Yanfei2',
+  '珊瑚宫心海': 'SangonomiyaKokomi',
+  '珐露珊': 'Faruzan',
+  '班尼特': 'Bennett',
+  '班尼特2': 'Bennett2',
+  '琴 (1)': 'Jean1',
+  '琴 (2)': 'Jean2',
+  '瑶瑶': 'Yaoyao',
+  '甘雨 (1)': 'Ganyu1',
+  '甘雨 (2)': 'Ganyu2',
+  '申鹤 (2)': 'Shenhe2',
+  '申鹤': 'Shenhe',
+  '白术': 'Baizhu',
+  '砂糖': 'Sucrose',
+  '神里绫人': 'KamisatoAyato',
+  '神里绫华': 'KamisatoAyaka',
+  '神里绫华2': 'KamisatoAyaka2',
+  '空': 'Aether',
+  '米卡': 'Mika',
+  '纳西妲': 'Nahida',
+  '罗莎莉亚 (2)': 'Rosaria2',
+  '罗莎莉亚': 'Rosaria',
+  '胡桃': 'HuTao',
+  '艾尔海森': 'Alhaitham',
+  '芭芭拉 (2)': 'Barbara2',
+  '芭芭拉': 'Barbara',
+  '荒泷一斗': 'AratakiItto',
+  '荧': 'Lumine',
+  '荧2': 'Lumine2',
+  '莫娜': 'Mona',
+  '莫娜2': 'Mona2',
+  '莱依拉': 'Layla',
+  '菲谢尔 (1)': 'Fischl1',
+  '菲谢尔 (2)': 'Fischl2',
+  '蔡诺': 'Tsaritsa',
+  '行秋': 'Xingqiu',
+  '行秋2': 'Xingqiu2',
+  '诺艾尔': 'Noelle',
+  '辛焱': 'Xinyan',
+  '辛焱2': 'Xinyan2',
+  '迪卢克 (1)': 'Diluc1',
+  '迪卢克 (2)': 'Diluc2',
+  '迪奥娜 (1)': 'Diona1',
+  '迪奥娜 (2)': 'Diona2',
+  '重云': 'Chongyun',
+  '重云2': 'Chongyun2',
+  '钟离': 'Zhongli',
+  '钟离2': 'Zhongli2',
+  '长野原宵宫': 'NaganoharaYoimiya',
+  '长野原宵宫2': 'NaganoharaYoimiya2',
+  '阿兹莫丹（天理）': 'Celestia',
+  '阿蕾奇诺': 'Arlecchino',
+  '阿贝多 (1)': 'Albedo1',
+  '阿贝多 (2)': 'Albedo2',
+  '雷泽 (2)': 'Razor2',
+  '雷泽': 'Razor',
+  '雷电将军': 'RaidenShogun',
+  '雷电将军2': 'RaidenShogun2',
+  '香菱': 'Xiangling',
+  '香菱2': 'Xiangling2',
+  '魈': 'Xiao',
+  '魈2': 'Xiao2',
+  '鹿野院平藏 (2)': 'ShikanoinHeizou2',
+  '鹿野院平藏': 'ShikanoinHeizou',
+  
+  // 崩坏星穹铁道角色
+  'Bronya': 'Bronya',
+  'Dehya': 'Dehya',
+  'Himeko': 'Himeko',
+  'Katheryne': 'Katheryne',
+  'Klara': 'Klara',
+  'Mimi': 'Mimi',
+  'Natasha': 'Natasha',
+  'Sampo': 'Sampo',
+  'Welt': 'Welt'
+}
+
+// 反向映射 - 英文名到中文名
+export const reverseMapping = Object.entries(modelNameMapping).reduce((acc, [cn, en]) => {
+  acc[en] = cn
+  return acc
+}, {})
+
+// 获取英文名
+export const getEnglishName = (chineseName) => {
+  return modelNameMapping[chineseName] || chineseName
+}
+
+// 获取中文名
+export const getChineseName = (englishName) => {
+  return reverseMapping[englishName] || englishName
+}
+
+// 生成模型列表数据
+export const generateModelList = () => {
+  return Object.entries(modelNameMapping).map(([cnName, enName], index) => ({
+    id: index + 1,
+    name: cnName,
+    englishName: enName,
+    filename: `${enName}.vrm`,
+    avatar: ['🌸', '⭐', '🌙', '💫', '🎀', '💎'][index % 6],
+    source: cnName.match(/Bronya|Dehya|Himeko|Katheryne|Klara|Mimi|Natasha|Sampo|Welt/) ? 'honkai' : 'genshin'
+  }))
+}
+
+export default modelNameMapping
