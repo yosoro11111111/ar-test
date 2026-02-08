343ddb773cf0798dfce4cc3428c8cab232843528
张三 <2869472750@qq.com>
2026-02-08 19:46:59 +0800

    refactor: remove old AR components and update routing to use new MMD Studio

--- src/main.jsx
+++ src/main.jsx
@@ -2,45 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-import { ARDirectorHome } from './components/ar/ARDirectorHome.jsx'
-import { ARSceneSelector } from './components/ar/ARSceneSelector.jsx'
-import { ARSceneCapture } from './components/ar/ARSceneCapture.jsx'
-import { ARTimelineEditorPage } from './components/ar/ARTimelineEditorPage.jsx'
-import { ARExportPage } from './components/ar/ARExportPage.jsx'
-import { ARCapturePro } from './components/ar/ARCapturePro/index.jsx'
-import { ARSceneManager } from './components/ar/ARSceneManager/index.jsx'
-import { ARDirector } from './components/ar/ARDirector/index.jsx'
-import { ARExportPro } from './components/ar/ARExportPro/index.jsx'
-import { ARImageImport } from './components/ar/ARImageImport/index.jsx'
-import { ARMMDDirector } from './components/ar/ARMMDDirector/index.jsx'
-import { ARSceneRecorderNew } from './components/ar/ARSceneRecorderNew.jsx'
-import { MobileRecordPage } from './components/ar/MobileRecordPage.jsx'
-import { WebXRARSceneRecorder } from './components/ar/ARMMDDirector/WebXRARSceneRecorder.jsx'
+import { MMDStudio } from './components/mmd/MMDStudio'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/ar-director" element={<ARDirectorHome />} />
-        
-        {/* 新版AR Director流程 */}
-        <Route path="/ar-director/selector" element={<ARSceneSelector />} />
-        <Route path="/ar-director/capture" element={<ARSceneCapture />} />
-        <Route path="/ar-director/capture-pro" element={<ARCapturePro />} />
-        <Route path="/ar-director/manager" element={<ARSceneManager />} />
-        <Route path="/ar-director/director/:sceneId" element={<ARDirector />} />
-        <Route path="/ar-director/export-pro/:sceneId" element={<ARExportPro />} />
-        <Route path="/ar-director/import-image" element={<ARImageImport />} />
-        <Route path="/ar-director/mmd" element={<ARMMDDirector />} />
-        <Route path="/ar-director/record" element={<WebXRARSceneRecorder isOpen={true} onClose={() => window.location.href='/ar-director'} onSceneRecorded={(data) => console.log('场景录制完成:', data)} />} />
-        <Route path="/ar-director/mobile-record" element={<MobileRecordPage />} />
-        
-        {/* 旧版兼容 */}
-        <Route path="/ar-director/edit/:projectId" element={<ARTimelineEditorPage />} />
-        <Route path="/ar-director/edit/new" element={<ARTimelineEditorPage />} />
-        <Route path="/ar-director/export/:projectId" element={<ARExportPage />} />
+        <Route path="/" element={<MMDStudio />} />
+        <Route path="/mmd" element={<MMDStudio />} />
+        <Route path="/studio" element={<MMDStudio />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>,
